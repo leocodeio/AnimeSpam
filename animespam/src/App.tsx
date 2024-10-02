c5528f097962da8c26e59ad5efdabc3059264aa4
leodoeio <basicleo787@gmail.com>
2024-10-02 14:44:29 +0530

    will play with flowbite

--- animespam/src/App.tsx
+++ animespam/src/App.tsx
@@ -1,9 +1,17 @@
 import React from "react";
 import Slider from "./main/Slider";
+import Header from "./main/Header";
+
 const App: React.FC = () => {
   return (
-    <div className="flex items-center justify-center">
-      <Slider />
+    <div className="bg-gray-900 h-screen">
+      <Header />
+      
+
+      <div className="flex items-center justify-center">
+        <Slider />
+      </div>
+      
     </div>
   );
 };
